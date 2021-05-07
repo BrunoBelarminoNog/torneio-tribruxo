063fbf80ddde27f2d40860d121bbcb54532ebe1c
BrunoBelarminoNog <brunobelarm.nog@gmail.com>
2021-05-07 17:16:44 -0300

    fix: erro de ortografia

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,66 @@
 import { Component } from "react";
 import { NavLink } from "react-router-dom";
-import {gsap} from 'gsap';
+import { gsap } from "gsap";
 
 import "./style.css";
 
 class Header extends Component {
-  myHeader = null
-  myLogo = null
-  myNav = []
+  myHeader = null;
+  myLogo = null;
+  myNav = [];
 
   componentDidMount() {
-    gsap.timeline()
-      .from(this.myHeader, {duration: 1, translateX: -2000})
-      .to(this.myHeader, {duration: 1, backgroundColor: "#000000be"}, "-=.2")
-      .from(this.myLogo, {duration: 1, opacity: 0})
-      .from(this.myNav, {duration: 1.5, translateY: -400, stagger: {
-        amount: .5
-      }}, "-=2.5")
+    gsap
+      .timeline()
+      .from(this.myHeader, { duration: 1, translateX: -2000 })
+      .to(this.myHeader, { duration: 1, backgroundColor: "#000000be" }, "-=.2")
+      .from(this.myLogo, { duration: 1, opacity: 0 })
+      .from(
+        this.myNav,
+        {
+          duration: 1.5,
+          translateY: -400,
+          stagger: {
+            amount: 0.5,
+          },
+        },
+        "-=2.5"
+      );
   }
 
   render() {
     return (
-      <header ref={header => this.myHeader = header}>
+      <header ref={(header) => (this.myHeader = header)}>
         <div className="header-container">
-          <div className="logo" ref={div => this.myLogo = div}>HP</div>
+          <div className="logo" ref={(div) => (this.myLogo = div)}>
+            HP
+          </div>
 
-          <nav >
-            <NavLink to="/" exact activeClassName={'active'} ref={nav => this.myNav[0] = nav}>Ínicio</NavLink>
-            <NavLink to="/torneio" exact activeClassName={'active'} ref={nav => this.myNav[1] = nav}>Torneio</NavLink>
-            <NavLink to="/personagens" exact activeClassName={'active'} ref={nav => this.myNav[2] = nav}>Personagens</NavLink>
+          <nav>
+            <NavLink
+              to="/"
+              exact
+              activeClassName={"active"}
+              ref={(nav) => (this.myNav[0] = nav)}
+            >
+              Início
+            </NavLink>
+            <NavLink
+              to="/torneio"
+              exact
+              activeClassName={"active"}
+              ref={(nav) => (this.myNav[1] = nav)}
+            >
+              Torneio
+            </NavLink>
+            <NavLink
+              to="/personagens"
+              exact
+              activeClassName={"active"}
+              ref={(nav) => (this.myNav[2] = nav)}
+            >
+              Personagens
+            </NavLink>
           </nav>
         </div>
       </header>
